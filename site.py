b3fd209eb1a39ca6f7aa3bac6c832e154f8a5921
henrylin2008 <henrylin2008@yahoo.com>
2019-06-03 12:07:32 -0700

    tempaltes

--- site.py
+++ site.py
@@ -1,11 +1,16 @@
-from flask import Flask
+from flask import Flask, render_template
 
 
 app=Flask(__name__)
 
 @app.route('/')
 def home():
-    return "Welcome to my site!"
+    return render_template("home.html")
+
+@app.route('/about/')
+def about():
+    return "This is the About page!"
+
 
 if __name__ == "__main__":
     app.run(debug=True)
